e19e049a3c3c741d05b1a367568a7c2456b029d1
Adam Dangoor <adamdangoor@gmail.com>
2016-11-27 14:53:39 +0000

    Put command in function

--- virtualfish-prompt.fish
+++ virtualfish-prompt.fish
@@ -1,5 +1,5 @@
-if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+function fish_prompt
+    if set -q VIRTUAL_ENV
+	echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
 end
-
-echo -n -s (set_color -b blue white) "(" no venv ")" (set_color normal) " "
