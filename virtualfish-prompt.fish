4350b725711413c82f888bc67d37ecebe56167aa
Adam Dangoor <adamdangoor@gmail.com>
2016-11-27 14:47:56 +0000

    Update virtualfish-prompt.fish

--- virtualfish-prompt.fish
+++ virtualfish-prompt.fish
@@ -1,3 +1,5 @@
 if set -q VIRTUAL_ENV
     echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 end
+
+echo -n -s (set_color -b blue white) "(" no venv ")" (set_color normal) " "
