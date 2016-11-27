691df99ebdfe09b917138f25021a558bba5b660d
Adam Dangoor <adamdangoor@gmail.com>
2016-11-27 17:21:54 +0000

    Working solution

--- virtualfish_prompt.fish
+++ virtualfish_prompt.fish
@@ -1,8 +1,12 @@
 functions --copy fish_prompt default_fish_prompt
 
-function fish_prompt
+function virtualfish_prompt
 	if set -q VIRTUAL_ENV
 	    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 	end
 	default_fish_prompt
+end
+
+function fish_prompt
+	virtualfish_prompt
 end
